a5c2d22eb7601381ef58a541ef54120bf2f9a348
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-26 21:43:03 +0530

    Basic Core Programs

---
+++ Basic-Core-Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_Core_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+        }
+    }
+}
