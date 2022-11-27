b9a24dac5d1c1057a6a58ebbbf7de511a2b10160
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-27 20:01:39 +0530

    Flip Coin is Done

--- Basic-Core-Problems/Program.cs
+++ Basic-Core-Problems/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Basic Core Programs");
+            Console.WriteLine("Enter the number of times to flip coin");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Random random = new Random();
+            double headCount = 0, tailCount = 0;
+            for (int i = 0; i < num; i++)
+            {
+                double flip = random.NextDouble();
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    tailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    headCount++;
+                }
+            }
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Tails :" + tailCount);
+            Console.WriteLine("Head :" + headCount);
+            headCount *= (100 / num);
+            tailCount *= (100 / num);
+            Console.WriteLine("The Percentage of Heads vs Tails is {0}% and {1}%", headCount, tailCount);
+
         }
     }
 }
