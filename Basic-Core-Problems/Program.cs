98f7bffe1ad0640dcdeeed5a59a50bf58c3243eb
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-27 20:57:44 +0530

    Swap two number is Done

--- Basic-Core-Problems/Program.cs
+++ Basic-Core-Problems/Program.cs
@@ -4,16 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Dividend");
-            int dividend = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Divisor");
-            int divisor = Convert.ToInt32(Console.ReadLine());
-            int quotient = dividend / divisor;
-            int remainder = dividend % divisor;
-            Console.WriteLine("Quotient is : " + quotient);
-            Console.WriteLine("Remainder is : " + remainder);
-
-
+            Console.WriteLine("Enter the First number :");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number :");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            Console.WriteLine("After Swapping these Two Numbers : First number is = " + firstNumber + " and Second number is = " + secondNumber);
         }
     }
 }
